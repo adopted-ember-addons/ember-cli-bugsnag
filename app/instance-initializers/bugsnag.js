12a45c673cd4a765d68f1e0909bbd2e5403975d6
Alex Navasardyan <alex.navasardyan@gmail.com>
2016-01-18 09:47:11 -0800

    Uses getter instead of `lookup`
    Fixes #32

--- app/instance-initializers/bugsnag.js
+++ app/instance-initializers/bugsnag.js
@@ -11,25 +11,26 @@ export default {
     if (typeof Bugsnag === 'undefined') { return; }
 
     if (currentEnv !== 'test' && Bugsnag.notifyReleaseStages.indexOf(currentEnv) !== -1) {
+      let router = instance.container.lookup('router:main');
+
       Ember.onerror = function (error) {
-        Bugsnag.context = getContext(instance.lookup('router:main'));
+        Bugsnag.context = getContext(router);
         Bugsnag.notifyException(error);
         console.error(error.stack);
       };
 
       Ember.RSVP.on('error', function(error) {
-        Bugsnag.context = getContext(instance.lookup('router:main'));
+        Bugsnag.context = getContext(router);
         Bugsnag.notifyException(error);
         console.error(error.stack);
       });
 
       Ember.Logger.error = function (message, cause, stack) {
-        Bugsnag.context = getContext(instance.lookup('router:main'));
+        Bugsnag.context = getContext(router);
         Bugsnag.notifyException(generateError(cause, stack), message);
         console.error(stack);
       };
 
-      const router = instance.lookup('router:main');
       const originalDidTransition = router.didTransition || Ember.K;
       router.didTransition = function() {
         Bugsnag.refresh();
