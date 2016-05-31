7420b94a40d87676e1425242a1d90113b7d29996
Ben Holmes <mail@benholmes.uk>
2016-05-31 13:24:21 -0500

    Move bugsnag initialization to instance initializer
    Notably drops support for library url now importing through bower.

--- app/instance-initializers/bugsnag.js
+++ app/instance-initializers/bugsnag.js
@@ -2,6 +2,7 @@ import Ember  from 'ember';
 import config from '../config/environment';
 import { getContext, generateError } from 'ember-cli-bugsnag/utils/errors';
 import { getMetaData } from '../utils/bugsnag';
+import Bugsnag from 'bugsnag';
 
 var currentEnv = config.environment;
 
@@ -9,7 +10,42 @@ export default {
   name: 'bugsnag-error-service',
 
   initialize: function(instance) {
+
     if (typeof Bugsnag === 'undefined') { return; }
+
+    var content = '';
+    var bugsnagConfig;
+    var envArray;
+
+    if (!config.bugsnag) {
+      console.warn('`config.bugsnag` is not defined, using environment variables instead.');
+
+      var envApiKey = process.env['BUGSNAG_API_KEY'];
+      var envReleases = process.env['BUGSNAG_NOTIFY_RELEASE'];
+      var envEndpoint = process.env['BUGSNAG_ENDPOINT'];
+
+      if (!envApiKey || !envReleases) {
+        console.error('Environment variables `BUGSNAG_API_KEY` and `BUGSNAG_NOTIFY_RELEASE` are not specified. Bugsnag will not be injected.');
+
+        return '';
+      }
+
+      bugsnagConfig = {
+        apiKey: envApiKey,
+        notifyReleaseStages: envReleases.split(','),
+        releaseStage: process.env['BUGSNAG_RELEASE_STAGE'],
+        endpoint: envEndpoint
+      };
+    } else {
+      bugsnagConfig = config.bugsnag;
+    }
+
+    var releaseStage = bugsnagConfig.releaseStage || config.environment;
+    envArray = bugsnagConfig.notifyReleaseStages ? bugsnagConfig.notifyReleaseStages : [];
+
+    Object.keys(bugsnagConfig).forEach(function(key) {
+      Bugsnag[key] = bugsnagConfig[key];
+    });
 
     if (currentEnv !== 'test' && Bugsnag.notifyReleaseStages.indexOf(currentEnv) !== -1) {
       let owner = instance.lookup ? instance : instance.container;
--- index.js
+++ index.js
@@ -15,58 +15,5 @@ module.exports = {
         'bugsnag': ['default']
       }
     });
-  },
-
-  contentFor: function(type, config) {
-    var content = '';
-    var bugsnagConfig;
-    var envArray;
-
-    if (type === 'head' && config.environment !== 'test') {
-      if (!config.bugsnag) {
-        console.warn('`config.bugsnag` is not defined, using environment variables instead.');
-
-        var envApiKey = process.env['BUGSNAG_API_KEY'];
-        var envReleases = process.env['BUGSNAG_NOTIFY_RELEASE'];
-        var envEndpoint = process.env['BUGSNAG_ENDPOINT'];
-
-        if (!envApiKey || !envReleases) {
-          console.error('Environment variables `BUGSNAG_API_KEY` and `BUGSNAG_NOTIFY_RELEASE` are not specified. Bugsnag will not be injected.');
-
-          return '';
-        }
-
-        bugsnagConfig = {
-          apiKey: envApiKey,
-          libraryUrl: process.env['BUGSNAG_LIBRARY_URL'],
-          notifyReleaseStages: envReleases.split(','),
-          releaseStage: process.env['BUGSNAG_RELEASE_STAGE'],
-          endpoint: envEndpoint
-        };
-      } else {
-        bugsnagConfig = config.bugsnag;
-      }
-
-      var libraryUrl = bugsnagConfig.libraryUrl || 'https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js';
-      var releaseStage = bugsnagConfig.releaseStage || config.environment;
-      envArray = bugsnagConfig.notifyReleaseStages ? bugsnagConfig.notifyReleaseStages : [];
-
-      content = [
-        '<script ',
-        'src="' + libraryUrl + '" ',
-        'data-appversion="' + config.currentRevision + '" ',
-        'data-apikey="' + bugsnagConfig.apiKey + '">',
-        '</script>',
-        '<script>',
-        'if (typeof Bugsnag !== "undefined") {',
-        'Bugsnag.releaseStage = "' + releaseStage + '";',
-        'Bugsnag.notifyReleaseStages = ["' + envArray.join('","') + '"];',
-        bugsnagConfig.endpoint ? 'Bugsnag.endpoint = ["' + bugsnagConfig.endpoint + '"];' : '',
-        '}',
-        '</script>'
-      ];
-    }
-
-    return content;
   }
 };
