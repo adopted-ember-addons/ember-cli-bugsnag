846401226723c754a3b9bb8208982254de595961
Ben Holmes <mail@benholmes.uk>
2016-05-31 19:03:34 -0500

    Move configuration validation to separate file

--- app/initializers/ember-cli-bugsnag.js
+++ app/initializers/ember-cli-bugsnag.js
@@ -1,38 +1,27 @@
 import Ember  from 'ember';
 import config from '../config/environment';
 import Bugsnag from 'bugsnag';
+import BugsnagConfiguration from 'ember-cli-bugsnag/utils/bugsnag-configuration';
 
 export default {
   name: 'ember-cli-bugsnag',
 
   initialize: function(instance) {
-    let bugsnagConfig;
+    let configuration;
 
-    if (!config.bugsnag) {
+    if (config.bugsnag) {
+      configuration = new BugsnagConfiguration(config.bugsnag);
+    } else {
       console.info('[ember-cli-bugsnag] `config.bugsnag` is not defined, using environment variables instead.');
 
-      bugsnagConfig = {
+      configuration = new BugsnagConfiguration({
         apiKey: process.env['BUGSNAG_API_KEY'],
         notifyReleaseStages: process.env['BUGSNAG_NOTIFY_RELEASE'].split(','),
         releaseStage: process.env['BUGSNAG_RELEASE_STAGE'],
         endpoint: process.env['BUGSNAG_ENDPOINT']
-      };
-    } else {
-      bugsnagConfig = config.bugsnag;
+      });
     }
 
-    if (!bugsnagConfig.apiKey) {
-      console.error('[ember-cli-bugsnag] Bugsnag reporting could not be started because `apiKey` was not configred. Please add it to `bugsnag.apiKey` in `config/environment` or define environment variable `BUGSNAG_API_KEY`');
-      return;
-    }
-
-    if (!bugsnagConfig.notifyReleaseStages) {
-      console.warn('[ember-cli-bugsnag] Notify release stages not defined in configuration, defaulting to `["production"]`. Either define `bugsnag.notifyReleaseStages` in your config file or a comma separated environment variable BUGSNAG_NOTIFY_RELEASE');
-      bugsnagConfig.notifyReleaseStages = ["production"]
-    }
-
-    Object.keys(bugsnagConfig).forEach(function(key) {
-      Bugsnag[key] = bugsnagConfig[key];
-    });
+    configuration.apply(Bugsnag);
   }
 };
