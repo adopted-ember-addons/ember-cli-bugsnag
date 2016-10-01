bcb463280d9d3209d2b5bd2ca1de90b42ae5282d
Ben Holmes <mail@benholmes.uk>
2016-10-01 11:36:19 +0100

    Configure circle builds

--- testem.js
+++ testem.js
@@ -1,4 +1,33 @@
 /*jshint node:true*/
+
+function reportFormat() {
+  return _circleTestDirectory() ? 'xunit' : 'tap';
+}
+
+function outputFile() {
+  if (_circleTestDirectory()) {
+    return _ouputLocation(_circleTestDirectory(), _emberTryVersion());
+  }
+}
+
+function _ouputLocation(directory, version) {
+  var output = directory + '/testem';
+
+  if (version) {
+    output += '-' + version;
+  }
+
+  return output + '.xml';
+}
+
+function _circleTestDirectory() {
+  return process.env['CIRCLE_TEST_REPORTS'];
+}
+
+function _emberTryVersion() {
+  return process.env['EMBER_TRY_CURRENT_SCENARIO'];
+}
+
 module.exports = {
   "framework": "qunit",
   "test_page": "tests/index.html?hidepassed",
@@ -9,5 +38,8 @@ module.exports = {
   ],
   "launch_in_dev": [
     "Chrome"
-  ]
+  ],
+  reporter: reportFormat(),
+  report_file: outputFile(),
+  xunit_intermediate_output: true
 };
