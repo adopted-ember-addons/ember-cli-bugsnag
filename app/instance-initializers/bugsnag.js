00fb231351bae8b8c2d9594492ce8bea6273d5f6
Ivan Rodriguez <ivan.rodriguez.pulido@bbvaitspain.com>
2019-12-26 12:13:57 +0100

    feat: 🎸 propagacion no-errores

--- app/instance-initializers/bugsnag.js
+++ app/instance-initializers/bugsnag.js
@@ -2,13 +2,6 @@ import { setProperties, get } from '@ember/object';
 import Ember from 'ember';
 import * as appMethods from '../utils/bugsnag';
 
-class UnknownError extends Error {
-	constructor(message) {
-		super(message);
-		this.name = this.constructor.name;
-	}
-}
-
 function getContext(router) {
 	return `${router.currentRouteName} (${router.currentURL})`;
 }
@@ -49,12 +42,8 @@ export default {
 	},
 
 	_onError(error) {
-		if (!error) {
-			return;
-		}
-
 		if (!error || typeof error !== 'object' || !error.name || !error.message) {
-			error = this._formatUnknownError(error);
+			return;
 		}
 
 		this._setContext();
@@ -67,10 +56,6 @@ export default {
 		if (Ember.testing) {
 			throw error;
 		}
-	},
-
-	_formatUnknownError(message) {
-		return new UnknownError(message);
 	},
 
 	_setUser() {
