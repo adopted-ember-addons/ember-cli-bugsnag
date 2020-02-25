6724f6757dc8a6fb24de92b92c3ed442924700b3
Michael Deitcher <michaeldeitcher@gmail.com>
2020-02-24 22:03:35 -0700

    Update addon/utils/errors.js
    handle versions prior to 2.15
    
    Co-Authored-By: John Derr <jderr-mx@users.noreply.github.com>

--- addon/utils/errors.js
+++ addon/utils/errors.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export function getContext(router) {
   var url = router.get('location').getURL();
-  var routeName = router.currentRoute.name;
+  var routeName;
+  if(Ember.VERSION >= '2.15') {
+    routeName = router.currentRouteName;
+  } else {
+    routeName = infos[infos.length - 1].name;
+  }
 
   var firstSegments = routeName.replace(".index", "").replace(/\./g, ' ');
   var prettyRouteName = Ember.String.capitalize(firstSegments);
